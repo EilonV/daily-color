04c05e0a15f011cfd2804f4284e4ecd502c1d846
EilonV <73593619+EilonV@users.noreply.github.com>
2023-04-28 13:19:47 +0300

    added font, added homepage

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,11 +6,15 @@ import { useDispatch, useSelector } from "react-redux";
 export const Gallery = (dailyColor) => {
     const dispatch = useDispatch()
     const { galleryPics } = useSelector((state) => state.color)
+    const btnRef = useRef()
 
     const client = createClient('GvghfEPVmBVVTHflnWdzSNSFaI9eSeUpsGy16Gh8YYo5aSzxUJy8HVUF');
-    const query = 'nature'
+    let prevPicClick = ''
 
     useEffect(() => {
+        const options = ['nature', 'abstract', 'texture', 'pattern', 'space', 'technology', 'flowers', 'sky']
+        const query = options[Math.floor(Math.random() * options.length)]
+        console.log(query);
         const color = dailyColor.dailyColor.substring(1)
         let colorEl = document.querySelector('.gallery-wrapper');
         colorEl.style.background = `linear-gradient(180deg, ${dailyColor.dailyColor} 0%, white 100%)`
@@ -18,13 +22,27 @@ export const Gallery = (dailyColor) => {
         client.photos.search({ query, color, per_page: 15 }).then(photos => {
             dispatch(changeGallery(photos))
         });
-    }, [dailyColor]);
+    }, [dailyColor.dailyColor]);
 
-    // console.log('galleryPics', galleryPics);
+    const enlarge = (pic) => {
+        console.log(btnRef.current.style.opacity);
+        if (prevPicClick) {
+            prevPicClick.classList.remove('active')
+        }
+        let currPic = document.querySelector('.pic' + pic.id)
+        currPic.classList.add('active')
+        prevPicClick = currPic
+
+    }
+
     return <section className="gallery-wrapper main-layout">
         <div className="gallery">
             {galleryPics.photos && galleryPics.photos.map((pic) => {
-                return <img src={pic.src.large} alt="" />
+                return <div className="pic-wrapper">
+                    <img src={pic.src.large} className={'pic ' + 'pic' + pic.id} alt="" key={pic.id} onClick={(event) => enlarge(pic)} />
+                    <button ref={btnRef}></button>
+                </div>
+
             })}
         </div>
     </section>
